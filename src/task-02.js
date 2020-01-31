c2320c1cdd21019380efd8ba3e9e294219a00daa
Betrayer <StarDestroyer@ukr.net>
2020-01-31 12:03:21 +0200

    hw-06_in_progress

--- src/task-02.js
+++ src/task-02.js
@@ -3,7 +3,7 @@
 import users from "./users.js";
 
 const getUsersWithEyeColor = (users, color) => {
-  return users.filter(user => user.eyeColor === "color");
+  return users.filter(user => user.eyeColor === color);
 };
 
 console.log(getUsersWithEyeColor(users, "blue"));
